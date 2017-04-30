12387464560291f0aed01b80d9f8270b38d2bba8
Juaki97 <garcia.jjimenez@gmail.com>
2017-04-30 14:20:18 +0200

    Elección entre prestamo y revisión.

--- src/me/jojigarcia/controller/AppInterface.java
+++ src/me/jojigarcia/controller/AppInterface.java
@@ -1,5 +1,9 @@
 package me.jojigarcia.controller;
 
+import me.jojigarcia.model.Available;
+import me.jojigarcia.model.Book;
+import me.jojigarcia.model.Daily;
+
 import java.util.Scanner;
 
 /**
@@ -19,13 +23,57 @@ public class AppInterface {
 
         switch (menuOption){
             case 1:
+                selectLoan();
                 break;
             case 2:
                 break;
             default:
                 break;
         }
+    }
 
-        
+    private static void toLoan(){
+
+    }
+
+    private static Available selectLoan(){
+        Scanner input = new Scanner (System.in);
+        int option;
+        Available available;
+        int code;
+        String title;
+        int year;
+
+        do {
+            System.out.println("1. Libros");
+            System.out.println("2. Diarios");
+            System.out.println("Qué desea alquilar? ");
+            option = input.nextInt();
+        }while (option != 1 && option != 2);
+
+        System.out.println("Inserte el título: ");
+        title = input.nextLine();
+
+        System.out.println("Inserte el código: ");
+        code = input.nextInt();
+
+        System.out.println("Inserte el año: ");
+        year = input.nextInt();
+
+        switch (option){
+            case 1:
+                available = new Book(code,title,year);
+                break;
+            case 2:
+                available = new Daily(code,title,year);
+                break;
+            default:
+                available = new Book(code,title,year);
+                break;
+        }
+
+        System.out.println(available);
+
+        return available;
     }
 }
--- src/me/jojigarcia/model/Daily.java
+++ src/me/jojigarcia/model/Daily.java
@@ -3,5 +3,11 @@ package me.jojigarcia.model;
 /**
  * Created by joaquinjimenezgarcia on 27/4/17.
  */
-public class Daily {
+public class Daily extends Available implements Loan{
+    public Daily() {
+    }
+
+    public Daily(int code, String title, int year) {
+        super(code, title, year);
+    }
 }
